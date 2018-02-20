9b1773cd8264cedd77b0e45d644e7d4b40c7e0e1
DESKTOP-AFTLNVE\shibata <a@a.com>
2018-02-21 02:26:56 +0900

    slick

---
+++ js/rdeco.js
@@ -0,0 +1,27 @@
+﻿$(function(){
+    $("#header").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/header.html");
+    $("#footer").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/footer.html");
+    $('.slider').slick();
+})
+$(function() {
+    $('.multiple-item').slick({
+          infinite: true,
+          dots:true,
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          responsive: [{
+               breakpoint: 768,
+                    settings: {
+                         slidesToShow: 3,
+                         slidesToScroll: 3,
+               }
+          },{
+               breakpoint: 480,
+                    settings: {
+                         slidesToShow: 2,
+                         slidesToScroll: 2,
+                    }
+               }
+          ]
+     });
+});
