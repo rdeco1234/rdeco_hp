978770d48e6a57735a90c1bfc4968379f84e192a
DESKTOP-AFTLNVE\shibata <a@a.com>
2018-02-26 00:31:47 +0900

    こみっと

--- js/rdeco_load.js
+++ js/rdeco_load.js
@@ -1,6 +1,4 @@
 ﻿$(function(){
     $("#header").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/header.html");
-})
-$(function(){
     $("#footer").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/footer.html");
 })
