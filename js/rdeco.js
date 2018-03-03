3d444ed8f4201ff735d5a5963d13be52d5651970
DESKTOP-AFTLNVE\shibata <a@a.com>
2018-03-04 03:35:11 +0900

    かるせる

--- js/rdeco.js
+++ js/rdeco.js
@@ -14,8 +14,8 @@
           },{
                breakpoint: 480,
                     settings: {
-                         slidesToShow: 2,
-                         slidesToScroll: 2,
+                         slidesToShow: 1,
+                         slidesToScroll: 1,
                     }
                }
           ]
