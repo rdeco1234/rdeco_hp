46063f8443d390009a01e99ac407d569a2d45e6f
DESKTOP-AFTLNVE\shibata <a@a.com>
2018-03-12 21:06:47 +0900

    GA設定

--- js/rdeco_load.js
+++ js/rdeco_load.js
@@ -1,4 +1,5 @@
 ﻿$(function(){
     $("#rdeco-header").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/header.html");
     $("#rdeco-footer").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/footer.html");
+    $("#rdeco-tag-manager").load("/rdeco.sakura.ne.jp/htdocs/rdeco_hp/tag-manager.html");
 })
